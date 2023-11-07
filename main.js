2b768750763e81dcdf2d1db1b0b1440f6a384304
KruegerRafaelAprendiz <150095940+KruegerRafaelAprendiz@users.noreply.github.com>
2023-11-07 00:49:21 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-function addNewtask() {
+function addNewTask() {
     var list = document.getElementById("list");
     var text = document.getElementById("task_name").value;
 
     if (text.length === 0) {
         alert('Tarefa precisa ser digitada');
-    return;
-}
+        return;
+    }
 
     var listItem = document.createElement("li");
     listItem.className = "list-item";
 
-    const textElement = document.createTextNode(Text);
+    const textElement = document.createTextNode(text); // Corrigi o 'Text' para 'text'
     listItem.appendChild(textElement);
     list.appendChild(listItem);
 }
